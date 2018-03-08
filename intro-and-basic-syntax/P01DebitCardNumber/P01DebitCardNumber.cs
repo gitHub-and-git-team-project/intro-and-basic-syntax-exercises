c46eb294a6e8b291558911682ad25ffdc67deff5
AleksandarBoev <aleksandarboev95@gmail.com>
2018-03-08 23:13:30 +0200

    Added problem 1 solution

---
+++ intro-and-basic-syntax/P01DebitCardNumber/P01DebitCardNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P01DebitCardNumber
+{
+    class P01DebitCardNumber
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            int number4 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("{0:D4} {1:D4} {2:D4} {3:D4}", number1, number2, number3, number4);
+
+            //main ends here
+        }
+    }
+}
