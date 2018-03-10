e28c52c76f1f25fb8c763c93beea33c7bc6f6e24
Angel Jordanov <jordanov_angel@yahoo.com>
2018-03-11 01:51:35 +0200

    commented problem 1 and problem 3

--- intro-and-basic-syntax/exercise/P03MilesToKm/P03MilesToKm/Program.cs
+++ intro-and-basic-syntax/exercise/P03MilesToKm/P03MilesToKm/Program.cs
@@ -6,8 +6,11 @@ namespace ProductCalculations
     {
         static void Main(string[] args)
         {
+            // We receive the value in miles
             double miles = double.Parse(Console.ReadLine());
+            // We calculate the value in kilometers
             double kilometers = miles * 1.60934;
+            //We print on the console the result
             Console.WriteLine($"{kilometers:f2}");
         }
     }
