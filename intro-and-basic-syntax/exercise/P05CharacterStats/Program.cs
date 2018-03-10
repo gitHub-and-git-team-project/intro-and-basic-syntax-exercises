863aa51b2ed25bc5b39ec0ec81b4dd8f8a9550bf
TanyaMineva <tanya.mnv@gmail.com>
2018-03-11 00:24:56 +0200

    test document

--- intro-and-basic-syntax/exercise/P05CharacterStats/Program.cs
+++ intro-and-basic-syntax/exercise/P05CharacterStats/Program.cs
@@ -6,13 +6,22 @@ namespace Problem05_CharacterStats
     {
         static void Main()
         {
-           string name = Console.ReadLine();
+            // We recieve from the Console the name of the game character -> a string
+            string name = Console.ReadLine();
+
+            /* In the next four lines, we recieve
+             * current health, maximum health,
+             * current energy and maximum energy -> as integer*/
             int currentHealth = int.Parse(Console.ReadLine());
             int maximumHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maximumEnergy = int.Parse(Console.ReadLine());
 
+            // We print the characters name
             Console.WriteLine($"Name: {name}");
+
+            /* We calculate and display the information about the characters
+             * health and energy */
             Console.WriteLine($"Health: |{new string('|',currentHealth)}{new string('.', maximumHealth - currentHealth)}|");
             Console.WriteLine($"Energy: |{new string('|',currentEnergy)}{new string('.', maximumEnergy - currentEnergy)}|");
 
