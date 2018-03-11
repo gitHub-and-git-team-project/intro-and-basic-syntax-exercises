d429567a338ddab1f39aca11d41fcbb424d8d844
Angel Jordanov <jordanov_angel@yahoo.com>
2018-03-12 01:47:46 +0200

    problem 5 - change variable

--- intro-and-basic-syntax/exercise/P05CharacterStats/Program.cs
+++ intro-and-basic-syntax/exercise/P05CharacterStats/Program.cs
@@ -7,23 +7,23 @@ namespace Problem05_CharacterStats
         static void Main()
         {
             // We recieve from the Console the name of the game character -> a string
-            string name = Console.ReadLine();
+            string player = Console.ReadLine();
 
             /* In the next four lines, we recieve
              * current health, maximum health,
              * current energy and maximum energy -> as integer*/
-            int currentHealth = int.Parse(Console.ReadLine());
-            int maximumHealth = int.Parse(Console.ReadLine());
-            int currentEnergy = int.Parse(Console.ReadLine());
-            int maximumEnergy = int.Parse(Console.ReadLine());
+            int currHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
 
             // We print the characters name
             Console.WriteLine($"Name: {name}");
 
             /* We calculate and display the information about the characters
              * health and energy */
-            Console.WriteLine($"Health: |{new string('|', currentHealth)}{new string('.', maximumHealth - currentHealth)}|");
-            Console.WriteLine($"Energy: |{new string('|', currentEnergy)}{new string('.', maximumEnergy - currentEnergy)}|");
+            Console.WriteLine($"Health: |{new string('|', currHealth)}{new string('.', maxHealth - currHealth)}|");
+            Console.WriteLine($"Energy: |{new string('|', currEnergy)}{new string('.', maxEnergy - currEnergy)}|");
 
         }
     }
