7119af73c79f07c1d866d85d5fd9ebf009c4298f
IvanKirilov <ivanmilchevk@gmail.com>
2018-03-11 15:50:07 +0200

    Added comments.

--- intro-and-basic-syntax/exercise/P04Beverage Labels/Program.cs
+++ intro-and-basic-syntax/exercise/P04Beverage Labels/Program.cs
@@ -9,15 +9,18 @@ namespace Beverage_Labels
     {
         static void Main(string[] args)
         {
+            //Data for our product
             var name = Console.ReadLine();
             var volume = int.Parse(Console.ReadLine());
             var energy = int.Parse(Console.ReadLine());
             var sugar = int.Parse(Console.ReadLine());
+         
 
+            //calculations
             var calories = (double)energy * volume / 100;
             var sugars = (double)volume * sugar / 100;
 
-
+            //output data
             Console.WriteLine($"{volume}ml {name}:");
             Console.WriteLine($"{calories}kcal, {sugars}g sugars");
         }
