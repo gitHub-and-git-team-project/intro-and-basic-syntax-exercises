99593b1003f8ff21fc54c5f8f7e234bee28cfc3f
IvanKirilov <ivanmilchevk@gmail.com>
2018-03-11 18:52:28 +0200

    Changed variable name.

--- intro-and-basic-syntax/exercise/P01DebitCardNumber/P01DebitCardNumber.cs
+++ intro-and-basic-syntax/exercise/P01DebitCardNumber/P01DebitCardNumber.cs
@@ -7,13 +7,13 @@ namespace P01DebitCardNumber
         static void Main(string[] args)
         {
             // In the next four lines we receive four numbers 
-            int numberOne = int.Parse(Console.ReadLine());
-            int number2 = int.Parse(Console.ReadLine());
+            int number1 = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
             int number3 = int.Parse(Console.ReadLine());
             int number4 = int.Parse(Console.ReadLine());
 
             // We print on the console full debit card number
-            Console.WriteLine("{0:D4} {1:D4} {2:D4} {3:D4}", numberOne, number2, number3, number4);
+            Console.WriteLine("{0:D4} {1:D4} {2:D4} {3:D4}", number1, numberTwo, number3, number4);
 
             //main ends here
         }
